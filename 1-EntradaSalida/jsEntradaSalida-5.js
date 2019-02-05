4b3653fcc638d58a6f070aa9c7687ea28f5c074e
EmanuelSGarcia <44718682+EmanuelSGarcia@users.noreply.github.com>
2019-02-05 12:07:29 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,16 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
+	var nombre ;
+	var edad ;
+	nombre = prompt ("Ingrese su nombre");
+	edad= prompt ("ingrese su edad");
 	
-}
+	document.getElementById('elNombre').value = nombre;
+	document.getElementById('laEdad').value = edad;
 
+	alert("Usted es: " + nombre + " Su edad es: " + edad)
+	alert("¡Wellcome!")
+
+
+}

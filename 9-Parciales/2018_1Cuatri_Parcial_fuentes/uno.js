0c0c52b72fa742a8a4c8c03fad86ca7f91103e3b
Emanuel Garcia <44718682+EmanuelSGarcia@users.noreply.github.com>
2019-02-13 09:07:38 -0300

    Primer ejercicio parcial.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
-{
+{	
+ 	var xxxx1;
+ 	var xxxx2;
+ 	var resultado;
 
+ 	xxxx1 = prompt("Por favor introduzca su edad: " , "Edad.");
+ 	xxxx2 = prompt("Por favor introduzca su edad: " , "Edad.");
+
+ 	xxxx1 = parseInt(xxxx1);
+ 	xxxx2 = parseInt(xxxx2);
+ 	resultado = xxxx1 + xxxx2;
+ 	alert("La suma de sus edades es: " + resultado);
 }

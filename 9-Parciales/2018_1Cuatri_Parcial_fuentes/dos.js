86350f060b65c8cd8754b0282def0854dc129f10
Emanuel Garcia <44718682+EmanuelSGarcia@users.noreply.github.com>
2019-02-13 09:25:44 -0300

    Ejercicio 2 completo.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,14 @@
 function mostrar()
-{
-  
+{	
+ 	var precio; 
+ 	var resultadoTarjeta;
+ 	var resultadoFinal;
+
+ 	precio = document.getElementById('elNombre').value;
+ 	precio = parseInt(precio);
+
+ 	resultadoTarjeta = precio * 10 / 100;
+ 	resultadoFinal = precio + resultadoTarjeta;
+ 	alert("El precio del producto en efectivo es: $" + precio + "; con tarjeta tiene un recargo del 10% que seria $" + resultadoTarjeta + " de recargo, costandole final $" + resultadoFinal);
+
 }

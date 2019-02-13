b9cf0134a51de1a1bf10825eb526b0e37f88c54c
Emanuel Garcia <44718682+EmanuelSGarcia@users.noreply.github.com>
2019-02-13 12:23:03 -0300

    Clase 13/02/2019
    Falta corrección de errores + optimizar del código.

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,26 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numeroRandom;	
+	var mensaje;
+
+	numeroRandom= Math.floor((Math.random() * 10) + 1);
+	numeroRandom= parseInt(numeroRandom);
+
+	
+	if (numeroRandom > 8)
+	{
+		mensaje= " EXCELENTE";
+	}
+	else 
+		if (numeroRandom < 4) 
+		{
+			mensaje= " Vamos, la proxima se puede";
+		}
+		else 
+			{
+				mensaje= " APROBÓ";
+			}
+	alert(numeroRandom + mensaje);
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,71 @@
 function mostrar()
-{
+{	
+	var cantidad;
+	var precio;
+	var descuento;
+	var precioDescuento;
+	var tipoDePago;
+	var recargo;
+	var recargoFinal;
+	
+	cantidad = prompt("¿Cuantos productos compro?");
+	cantidad = parseInt(cantidad)
 
+	precio = prompt("Ingrese el precio de su/s productos sumados: ");
+	precio = parseInt(precio);
+
+	if (cantidad ==1 ) 
+	{
+
+		tipoDePago = prompt("¿Que medio de pago utilizara?" , "Tarjeta/Efectivo");
+		
+		if (tipoDePago == "Tarjeta")
+		{ 	
+
+			else{alert("Usted estaria abonando: " + precio);
+				
+			}	
+		}		
+	}
+
+	if (cantidad > 1 && precio < 1999) 
+	{
+
+		descuento = precio * 10 / 100;
+		descuento = parseInt(descuento);
+
+		precioDescuento= precio - descuento;
+
+		alert("Usted estaria abonando: " + precio +" ,Aplicando su descuento valido del 10%, terminaria pagando: " + precioDescuento)
+
+	}
+
+	if(cantidad > 1 && precio >= 2000)
+	 {
+	 	tipoDePago = prompt("¿Que medio de pago utilizara?" , "Tarjeta/Efectivo");
+
+	 	if (tipoDePago == "Efectivo") 
+	 	{
+	 		
+		descuento = precio * 15 / 100;
+		descuento = parseInt(descuento);
+
+		precioDescuento= precio - descuento;
+
+		alert("Usted estaria abonando: " + precio +" ,Aplicando su descuento valido del 15%, terminaria pagando: " + precioDescuento)
+	 	}
+	 	if (tipoDePago == "Tarjeta") 
+	 	{
+	 		descuento = precio * 15 / 100;
+		descuento = parseInt(descuento);
+
+		precioDescuento= precio - descuento;
+		recargo= precio *10/100;
+		recargo= parseInt(recargo);
+
+		recargoFinal = precio + recargo
+
+		alert("Usted estaria abonando: " + precio +" ,Aplicando su descuento valido del 15%, terminaria pagando: " + precioDescuento +" .Al abonar con tarjeta recive una recarga de: " + recargo +" ,Con un precio final de: " +recargoFinal);
+	 	}
+	 }
 }
